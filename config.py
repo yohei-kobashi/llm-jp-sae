53d4ec8a357a525f98d1990cad0f7ec799991ab0
yohei-kobashi <yohei.kobashi@gmail.com>
2025-08-05 09:27:43 +0900

    misc

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class SaeConfig(PretrainedConfig):
 @dataclass
 class TrainConfig:
     lr_warmup_steps: int = 1000
-    batch_size: int = 256
+    batch_size: int = 128
     inf_bs_expansion: int = 2
     logging_step: int = 50
 

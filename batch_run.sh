84d38e9df6f6ccb9e280b0ac31d4aedd3b50f65f
yohei-kobashi <yohei.kobashi@gmail.com>
2026-03-17 10:32:24 +0900

    misc

--- batch_run.sh
+++ batch_run.sh
@@ -10,6 +10,6 @@
 source ~/start_gpu_nodes.sh
 cd llm-jp-sae 
 
-bash run_lingualens_pipeline.sh --target will --model-path llm-jp/llm-jp-3-1.8 
-bash run_lingualens_pipeline.sh --target will --model-path ballenai/OLMo-2-0425-1B --sae-path-template olmo2_sae/n_d_16/k_32/nl_Scalar/ckpt_0988240/lr_0.001/sae_layer{}.pth
+bash run_lingualens_pipeline.sh --target will --model-path llm-jp/llm-jp-3-1.8b
+bash run_lingualens_pipeline.sh --target will --model-path allenai/OLMo-2-0425-1B --sae-path-template olmo2_sae/n_d_16/k_32/nl_Scalar/ckpt_0988240/lr_0.001/sae_layer{}.pth
 # bash run_lingualens_pipeline.sh --target will,can,could,may,might,must,shall,should,would,ought to,suppose --model-path allenai/OLMo-2-0425-1B 

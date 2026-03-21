7c61cc015cc90e6fae4e87f8e8579e6ddf11f558
yohei-kobashi <yohei.kobashi@gmail.com>
2026-03-21 14:23:20 +0900

    misc

--- batch_run_qwen.sh
+++ batch_run_qwen.sh
@@ -10,5 +10,5 @@
 source ~/start_gpu_nodes.sh
 cd llm-jp-sae 
 
-bash run_lingualens_pipeline.sh --target will,can,could,may,might,must,should,would --model-path Qwen/Qwen2.5-1.5B  --sae-path-template qwen2.5_sae/n_d_16/k_32/nl_Scalar/ckpt_0988240/lr_0.001/sae_layer{}.pth --output-root outputs/Qwen2.5-1.5B
+# bash run_lingualens_pipeline.sh --target will,can,could,may,might,must,should,would --model-path Qwen/Qwen2.5-1.5B  --sae-path-template qwen2.5_sae/n_d_16/k_32/nl_Scalar/ckpt_0988240/lr_0.001/sae_layer{}.pth --output-root outputs/Qwen2.5-1.5B
 bash run_lingualens_pipeline.sh --target suppose --model-path Qwen/Qwen2.5-1.5B  --sae-path-template qwen2.5_sae/n_d_16/k_32/nl_Scalar/ckpt_0988240/lr_0.001/sae_layer{}.pth --output-root outputs/Qwen2.5-1.5B --input-jsonl data/minimal_pairs_acceptability_2.jsonl
